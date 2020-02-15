720513b23ceb1475062151cac75b24c9d7d163aa
TITAS MALLICK <44458514+titasmallick@users.noreply.github.com>
2020-02-15 19:04:09 +0530

    M

--- titas.js
+++ titas.js
@@ -5,14 +5,7 @@ self.addEventListener('install', function(e) {
  e.waitUntil(
    caches.open('Creativegun').then(function(cache) {
      return cache.addAll([
-        // '/',
-        '/PWA.html',
-        '/PWA.css',
-        '/titas.js',
-        '/main1.js',
-        '/main2.js',
-        '/assets/logo.svg',
-        '/assets/logo.png'
+        '/'
      ]);
    })
  );
